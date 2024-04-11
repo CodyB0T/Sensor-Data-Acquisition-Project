1b9aa3e141da885cadc51217da0c5f77c020eb9c
CodyB0T <59010772+CodyB0T@users.noreply.github.com>
2024-04-11 19:50:01 -0400

    got the hz

--- shimmer/accel.py
+++ shimmer/accel.py
@@ -1,6 +1,9 @@
 import serial
 import struct
 import sys
+import pandas as pd
+from datetime import datetime
+import time
 
 
 def wait_for_ack():
@@ -23,16 +26,25 @@ else:
     ser = serial.Serial(sys.argv[1], 115200)
     ser.flushInput()
     print("port opening, done.")
+
     # send the set sensors command
     ser.write(struct.pack("BBBB", 0x08, 0x80, 0x00, 0x00))  # analogaccel
     wait_for_ack()
     print("sensor setting, done.")
+
     # send the set sampling rate command
-    ser.write(
-        struct.pack("BBB", 0x05, 0x00, 0x19)
-    )  # 5.12Hz (6400 (0x1900)). Has to be done like this for alignment reasons
+
+    # ser.write(
+    #     struct.pack("BBB", 0x05, 0x00, 0x19)
+    #                              right, left
+    # )  # 5.12Hz (6400 = (0x1900)). Has to be done like this for alignment reasons
+    # 2^15 / 6400 = 5.12
+
+    ser.write(struct.pack("BBB", 0x05, 0x3E, 0x00))  # 550 hz
+
     wait_for_ack()
     print("sampling rate setting, done.")
+
     # send start streaming command
     ser.write(struct.pack("B", 0x07))
     wait_for_ack()
@@ -43,7 +55,10 @@ else:
     numbytes = 0
     framesize = 10  # 1byte packet type + 3byte timestamp + 3x2byte Analog Accel
 
+    xlog = []
+
     print("Packet Type,Timestamp,Analog Accel X,Analog Accel Y,Analog Accel Z")
+    start_time = time.time()
     try:
         while True:
             while numbytes < framesize:
@@ -66,9 +81,16 @@ else:
                 "0x%02x,%5d,\t%4d,%4d,%4d"
                 % (packettype[0], timestamp, analogaccelx, analogaccely, analogaccelz)
             )
+            xlog.append(analogaccelx)
+            # print(xlog)
+            # time.sleep(0.002)
 
     except KeyboardInterrupt:
         # send stop streaming command
+        finish_time = time.time()
+        time_elapsed = finish_time - start_time
+        print(time_elapsed)
+        pd.DataFrame(xlog).to_csv("data.csv", index=False)
         ser.write(struct.pack("B", 0x20))
         print
         print("stop command sent, waiting for ACK_COMMAND")
@@ -77,3 +99,7 @@ else:
         # close serial port
         ser.close()
         print("All done")
+
+        rounded_time = round(time_elapsed, 5)
+
+        print(len(xlog) / rounded_time)
